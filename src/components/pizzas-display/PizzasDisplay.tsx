0e918dd8681680ccdfc14122d74b1a826132cbd5
dmitry_perepelkin <dmitry_perepelkin@relex.ru>
2023-06-22 16:37:42 +0300

    added dataPizzas.json

--- src/components/pizzas-display/PizzasDisplay.tsx
+++ src/components/pizzas-display/PizzasDisplay.tsx
@@ -3,12 +3,11 @@ import {Pizza,PizzasDisp} from "../interfaces";
 import PizzaCard from "../pizza-card/PizzaCard";
 
 export const PizzasDisplay: FC<PizzasDisp> = ({pizzaList}) => {
-  console.log("pizzalIST", pizzaList)
   return (
       <div className="container">
 
         {pizzaList.map((pizzaItem: Pizza) => {
-            return pizzaItem.title
+            return <PizzaCard pizza={pizzaItem}/>
           }
         )}
       </div>
